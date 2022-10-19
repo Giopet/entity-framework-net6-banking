29140f4b9fbd27c582c7910e16c300d3a77fc42c
Giopet <petasgiorgos@gmail.com>
2022-10-20 00:21:18 +0300

    console logging added

--- EF6.Banking/EF6.Banking.Persistence/BankingDbContext.cs
+++ EF6.Banking/EF6.Banking.Persistence/BankingDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using EF6.Banking.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EF6.Banking.Persistence
 {
@@ -10,7 +11,9 @@ namespace EF6.Banking.Persistence
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=Banking_EF6");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=Banking_EF6")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information) // Everything happens, is going to be seen.
+                .EnableSensitiveDataLogging(); // Everything happens in the background that probably end-user must not see.
         }
 
         public DbSet<Tenant> Tenants { get; set; }
