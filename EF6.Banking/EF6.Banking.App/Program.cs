74f0163a6c22cf163494e67b49ca77a2f4bce5ab
Giopet <petasgiorgos@gmail.com>
2022-10-30 02:26:16 +0300

    Aggregate Filter Methods, Alternative Linq Syntax, Update Methods

--- EF6.Banking/EF6.Banking.App/Program.cs
+++ EF6.Banking/EF6.Banking.App/Program.cs
@@ -15,14 +15,25 @@ BankingDbContext context = new(); //old way: private static BankingDbContext con
 //await LoadTenantsWithSimpleSelectQuery();
 
 /* Filter Operation Methods */
-await LoadTenantsWithFilter();
+//await LoadTenantsWithFilter();
+
+/* Aggregate Filter Methods */
+//await ExecuteAdditionalFilters();
+
+/* Alternative Linq Syntax */
+//await AlternativeLinqSyntax();
+
+/* Update Methods */
+//await SimpleUpdateTenant();
+//await SimpleUpdateAccount();
+
 
 
 Console.WriteLine("Press Any Key for Application's Termination...");
 Console.ReadKey();
 
 
-async Task AddTenant(Tenant tenant) // old way: static
+async Task AddTenant(Tenant tenant) // old way: private static
 {
     context.Tenants.Add(tenant); // Tracked in memory until SaveChangesAsync called.
     await context.SaveChangesAsync(); // Generates the SQL, send it into the database and rollback if anything fails.
@@ -93,3 +104,79 @@ async Task LoadTenantsWithFilter()
         Console.WriteLine($"{tenant.Id} - {tenant.Name}");
     }
 }
+
+async Task ExecuteAdditionalFilters()
+{
+    var tenants = context.Tenants;
+    var list = await tenants.ToListAsync();
+    var first = await tenants.FirstAsync();
+    var firstOrDefault = await tenants.FirstOrDefaultAsync();
+    var single = await tenants.SingleAsync(); // possible exception
+    var singleOrDefault = await tenants.SingleOrDefaultAsync(); // possible exception
+
+    var count = await tenants.CountAsync();
+    var longCount = await tenants.LongCountAsync();
+    var min = await tenants.MinAsync(); // possible exception
+    var max = await tenants.MaxAsync(); // possible exception
+
+    // DbSet method that will execute - can be used with front-end passing this id
+    var tenant = await tenants.FindAsync(1);
+}
+
+async Task AlternativeLinqSyntax()
+{
+    Console.Write("Enter a Tenant Name (Or Part of): ");
+    var tenantName = Console.ReadLine();
+
+    var tenants = await (from t in context.Tenants
+                         where EF.Functions.Like(t.Name, $"%{tenantName}%") // or: t.Name.Contains()
+                         select t).ToListAsync(); // make IEnumerable fro IQuerable to have more methodss
+
+    foreach(var tenant in tenants)
+    {
+        Console.WriteLine($"{tenant.Id} - {tenant.Name}");
+    }
+}
+
+async Task GetModel()
+{
+    // Retrieve Model
+    var tenant = await context.Tenants.FindAsync(1);
+    Console.WriteLine($"{tenant.Id} - {tenant.Name}");
+}
+
+/// <summary>
+/// If update is already done , then if you update the same value no connection on db is opened
+/// EF uses tracking to achieve this. Keeps in the memory what changes has occured.
+/// </summary>
+async Task SimpleUpdateTenant()
+{
+    // Retrieve Model
+    var tenant = await context.Tenants.FindAsync(1);
+
+    // Make Model Changes
+    tenant.Name = "AA Bank";
+
+    // Save Changes
+    await context.SaveChangesAsync();
+
+    await GetModel();
+}
+
+async Task SimpleUpdateAccount()
+{
+    // If we dont specify the id, an insert operation will happen and go add a row with new id.
+    // If Id is wrong, will throw an exception.
+    var account = new Account
+    {
+        Id = 1, 
+        Name = "Money Market",
+        TenantId = 1
+    };
+
+    context.Accounts.Update(account);
+
+    await context.SaveChangesAsync();
+}
+
+
